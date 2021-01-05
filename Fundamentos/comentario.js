577935f1df3494c2600569dff4b7f482a4bacf82
tangriane <tangrianekolisnichenco@hotmail.com>
2021-01-05 11:16:12 -0300

    'master'

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,11 @@
+//comentarios
+console.log('Linha 1')
+/*
+Cometarios de 
+varias linhas
+*/
+console.log('linha 2')
+/*
+*linha
+*outra linha
+*/
---
+++ Fundamentos/variavesEConstantes.js
@@ -0,0 +1,13 @@
+var a = 3 //pode se redeclarado a variavel, isso pode ser pedigoso
+let b = 4 //uso sempre o let, evite o var 
+
+console.log(a,b)
+
+a = 300
+b = 400
+
+console.log(a,b)
+
+const c = 5 //não tem motivo para mudar, coloca contante(const)
+
+console.log(c)
